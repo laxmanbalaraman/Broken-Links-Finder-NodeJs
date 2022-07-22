4231830d0e3a7cd4e14ae420aef1be3d3cbef96c
laxmanbalaraman <laxmanbalaraman@gmail.com>
2022-07-22 15:10:09 +0530

    Server Sent Events

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import Search from "./components/search/Search";
+import { ssEvents } from "./config/config";
 import "./app.css";
 function App() {
   const [crawledCount, setCrawledCount] = useState(0);
   const [brokenLinksCount, setbrokenLinksCount] = useState(0);
   const [brokenLinks, setBrokenLinks] = useState("");
   const [showResult, setShowResult] = useState(false);
+
+  ssEvents.addEventListener("crawled", (e) => {
+    console.log(e.data);
+    setCrawledCount(e.data);
+  });
+
+  ssEvents.addEventListener("broken", (e) => {
+    console.log(e.data);
+    setbrokenLinksCount(e.data);
+  });
+
+  ssEvents.onerror = (e) => {
+    console.log("error occured", e);
+  };
 
   return (
     <div className="App">
---
+++ client/src/config/config.js
@@ -0,0 +1,2 @@
+export const STREAM_URL = `http://localhost:5000/stream`;
+export const ssEvents = new EventSource(STREAM_URL);
